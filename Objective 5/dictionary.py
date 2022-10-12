af63a5133a8c7b897f4701193289bf412715dbe8
Harry Chown <44265236+harrychown@users.noreply.github.com>
2022-10-12 21:27:02 +0100

    Add files via upload

---
+++ Objective 5/dictionary.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Objective 5 - Dictionary
+By Harry Chown
+"""
+{"Harry":"Chown",
+ "Bob":"Geldof",
+ "Ada":"Lovelace",
+ "Michelle":"Obama",
+ "Rosa":"Parks",
+ "Laura":"Boylan",
+ "Susie":"Chown",
+ "Elvis":"Presley",
+ "Guido":"Van Rossum",
+ "Diana":"Ross",
+ "Audrey" :"Hepburn",
+ "Boaty" : "McBoatface"
+ }
+    
